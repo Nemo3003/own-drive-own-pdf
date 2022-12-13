3092d5f59d1857557c275dad2b2e25eb870e8906
Nemo3003 <ulimarian2000@gmail.com>
2022-12-13 09:41:48 -0300

    [CREATE] Beginning new project

---
+++ index.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const app = express();
+app.use(express.json());
+const PORT = process.env.PORT || 8080;
+const mongodb = require("./src/db/connect");
+
+
+mongodb.initDb((err, mongodb) => {
+    if (err) {
+      console.log(err);
+    } else {
+      app.listen(PORT);
+      console.log(`Connected to port => ${PORT}`);
+    }
+  });
