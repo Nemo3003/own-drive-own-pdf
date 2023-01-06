71c5c3d8d03815862af5ff889f8c5435f7960184
Nemo3003 <ulimarian2000@gmail.com>
2023-01-06 19:29:59 -0300

    [UPD] A few pages added

--- src/routes/class.routes.js
+++ src/routes/class.routes.js
@@ -3,9 +3,34 @@ const express = require('express');
 const { model } = require('mongoose');
 const router = express.Router();
 
+// ***************** FIRST YEAR *****************
 router.get('/coneyone', (req, res) => {
     res.render('../views/pdf/anio/one/oneclassoneyear.hbs')
 })
-
+router.get('/coneytwo', (req, res) => {
+    res.render('../views/pdf/anio/one/twoclassoneyear.hbs')
+})
+router.get('/coneythree', (req, res) => {
+    res.render('../views/pdf/anio/one/threeclassoneyear.hbs')
+})
+router.get('/coneyfourth', (req, res) => {
+    res.render('../views/pdf/anio/one/fourthclassoneyear.hbs')
+})
+router.get('/coneyfifth', (req, res) => {
+    res.render('../views/pdf/anio/one/fifthclassoneyear.hbs')
+})
+router.get('/coneysix', (req, res) => {
+    res.render('../views/pdf/anio/one/sixclassoneyear.hbs')
+})
+router.get('/coneyseven', (req, res) => {
+    res.render('../views/pdf/anio/one/sevenclassoneyear.hbs')
+})
+router.get('/coneyeight', (req, res) => {
+    res.render('../views/pdf/anio/one/eightclassoneyear.hbs')
+})
+router.get('/coneynine', (req, res) => {
+    res.render('../views/pdf/anio/one/nineclassoneyear.hbs')
+})
+// ***************** SECOND YEAR *****************
 // Exports
 module.exports = router;
